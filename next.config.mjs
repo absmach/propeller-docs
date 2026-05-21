87721f4b3505c1ed2a6bcfda1f80ff767619f398
ianmuchyri <ianmuchiri8@gmail.com>
2026-05-21 12:13:53 +0300

    fix urls
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,13 @@ import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
-const basePath = "/docs/propeller";
-
 /** @type {import('next').NextConfig} */
 const config = {
-  basePath,
   output: "export",
   trailingSlash: true,
   reactStrictMode: true,
   images: {
     unoptimized: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
   },
 };
 
